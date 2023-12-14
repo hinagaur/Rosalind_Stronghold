5613eab257c9032c70120b34f4a157fa94892037
Hina Gaur <hinagaur99@gmail.com>
2023-12-14 15:34:50 -0500

    Adding new solutions and respective .txt files

---
+++ rosalind_PROT.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+#Making a dictionary 
+codon_table = {
+    "UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L",
+    "UCU":"S", "UCC":"S", "UCA":"S", "UCG":"S",
+    "UAU":"Y", "UAC":"Y", "UAA":"STOP", "UAG":"STOP",
+    "UGU":"C", "UGC":"C", "UGA":"STOP", "UGG":"W",
+    "CUU":"L", "CUC":"L", "CUA":"L", "CUG":"L",
+    "CCU":"P", "CCC":"P", "CCA":"P", "CCG":"P",
+    "CAU":"H", "CAC":"H", "CAA":"Q", "CAG":"Q",
+    "CGU":"R", "CGC":"R", "CGA":"R", "CGG":"R",
+    "AUU":"I", "AUC":"I", "AUA":"I", "AUG":"M",
+    "ACU":"T", "ACC":"T", "ACA":"T", "ACG":"T",
+    "AAU":"N", "AAC":"N", "AAA":"K", "AAG":"K",
+    "AGU":"S", "AGC":"S", "AGA":"R", "AGG":"R",
+    "GUU":"V", "GUC":"V", "GUA":"V", "GUG":"V",
+    "GCU":"A", "GCC":"A", "GCA":"A", "GCG":"A",
+    "GAU":"D", "GAC":"D", "GAA":"E", "GAG":"E",
+    "GGU":"G", "GGC":"G", "GGA":"G", "GGG":"G"}
+
+def RnaToProt(mrna: str)-> float:
+    
+    #Opening the file containing the mrna sequence
+    mrna_seq = open(mrna, "r").read().strip()
+
+    #Making a list of all the three letter codons in the sequence
+    codon_list = []
+    for i in range(0, len(mrna_seq), 3):
+        codon_list.append(mrna_seq[i:i+3])
+    
+    prot_str = ""
+    for j in codon_list:
+        prot_code = codon_table.get(j)
+        if j == "UAA" or j == "UAG" or j == "UGA":
+            break
+        elif prot_code == None:
+            pass
+        else:
+           prot_str += prot_code
+    return prot_str
+  
+
+if __name__ == '__main__':
+    print(RnaToProt("rosalind_data/rosalind_prot.txt"))

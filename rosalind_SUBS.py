cf9fa85fe3da4871db23930ddeb03706a0ef54fd
Hina Gaur <hinagaur99@gmail.com>
2023-12-15 14:39:04 -0500

    Adding a new solution for rosalind problem

---
+++ rosalind_SUBS.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def find_motif(DNA_string_1: str, DNA_string_2: str) -> list:
+    '''
+    Two DNA strings DNA_string_1 and DNA_string_2 (each of length at most 1 kbp) and returns
+    All locations of DNA_string_2  as a substring of DNA_string_1.
+    '''
+    return [n+1 for n in range(len(DNA_string_1)) if DNA_string_1.find(DNA_string_2 , n) == n]
+
+if __name__ == '__main__':
+    print(find_motif('GATATATGCATATACTT', 'ATAT'))
